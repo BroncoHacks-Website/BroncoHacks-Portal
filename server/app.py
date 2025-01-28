60cc96068506e7d785f058c87d025db25b8c5630
Caleb C <kaichun24@gmail.com>
2025-01-27 18:41:54 -0800

    added basic route, need to remove password from return

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import sqlite3
 
 
@@ -26,3 +26,23 @@ def urmom():
         return jsonify(status=200,message="urmom",hackers=posts_list)
     except Exception as e:
         return jsonify(status=400,message=str(e))
+    
+    
+@app.route("/oneHacker", methods=['GET'])
+def getOneHacker():
+    # get req param from url
+    uuid = request.args.get('uuid')
+    
+    try:
+        conn = get_db_connection()
+        hacker = conn.execute('SELECT * FROM hackers WHERE UUID=?', (uuid,)).fetchall()
+        conn.close
+        
+        hacker_list = [dict(row) for row in hacker]
+        
+        if len(hacker_list) == 0:
+            return jsonify(status=404, message="Hacker Not Found")
+        else:
+            return jsonify(status=200, message="Hacker Found", hacker=hacker_list)
+    except Exception as e:
+        return jsonify(status=400, message=str(e))
