349ed6a8e661bf2e69b40c0024baf479e873c70d
Jayden Nguyen <Jayden.vinh.nguyen@gmail.com>
2025-01-27 22:30:33 -0800

    update to new schema

--- server/init_db.py
+++ server/init_db.py
@@ -8,8 +8,8 @@ with open('schema.sql') as f:
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO hackers (UUID, teamID, firstName, lastName, password, email, school, discord, confirmationNumber, isConfirmed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-            ('Skibidi', 'ABCDEF', 'Daniel', 'Pasion', 'urmom', 'dpasion@cpp,edu', 'cpp','.theDaniel', '666420', True )
+cur.execute("INSERT INTO hackers (teamID, firstName, lastName, password, email, school, discord, confirmationNumber, isConfirmed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            ('ABCDEF', 'Daniel', 'Pasion', 'urmom', 'dpasion@cpp,edu', 'cpp','.theDaniel', '666420', True )
             )
 
 cur.execute("INSERT INTO teams (teamID, teamName, owner) VALUES (?, ?, ?)",
