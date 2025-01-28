7e4bfa3900b5113071b09f8b449ab456a4d302aa
Jayden Nguyen <Jayden.vinh.nguyen@gmail.com>
2025-01-27 23:26:50 -0800

    adapted to password encryption

--- server/init_db.py
+++ server/init_db.py
@@ -1,4 +1,10 @@
 import sqlite3
+import bcrypt
+
+def hash_password(password):
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed
 
 connection = sqlite3.connect('database.db')
 
@@ -9,7 +15,7 @@ with open('schema.sql') as f:
 cur = connection.cursor()
 
 cur.execute("INSERT INTO hackers (teamID, firstName, lastName, password, email, school, discord, confirmationNumber, isConfirmed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-            ('ABCDEF', 'Daniel', 'Pasion', 'urmom', 'dpasion@cpp,edu', 'cpp','.theDaniel', '666420', True )
+            ('ABCDEF', 'Daniel', 'Pasion', hash_password('urmom'), 'dpasion@cpp,edu', 'cpp','.theDaniel', '666420', True )
             )
 
 cur.execute("INSERT INTO teams (teamID, teamName, owner) VALUES (?, ?, ?)",
