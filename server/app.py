7b1a1d3ebad65db6b8a6b02c0e2404a7832fb841
Jayden Nguyen <Jayden.vinh.nguyen@gmail.com>
2025-01-27 23:02:13 -0800

    Added get hacker by id function and added email and discord unique check

--- server/app.py
+++ server/app.py
@@ -10,6 +10,16 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+def get_hacker_by_id(hacker_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT UUID, teamID, firstName, lastName, email, school, discord, confirmationNumber, isConfirmed FROM hackers WHERE UUID = ?", (hacker_id,))
+    hacker = cursor.fetchone()
+    conn.close()
+    if hacker:
+        return dict(hacker)
+    return None
+
 @app.route("/")
 def index():
     return "<p>Server is Running :)</p>"
@@ -37,19 +47,35 @@ def create_hacker():
         password = data['password']
         email = data['email']
         school = data['school']
-        discord = data.get('discord', "")
-        teamID = ""
-        confirmationNumber = ""
+        discord = data.get('discord', None)
+        teamID = None
+        confirmationNumber = None
         isConfirmed = False
         hacker_uuid = str(uuid.uuid4())
     
         conn = get_db_connection()
-        conn.execute("INSERT INTO hackers (teamID, firstName, lastName, password, email, school, discord, confirmationNumber, isConfirmed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", 
-               (teamID, firstName, lastName, password, email, school, discord, confirmationNumber, isConfirmed))
+        cursor = conn.cursor()
 
+        cursor.execute("SELECT * FROM hackers WHERE email = ?", (email,))
+        if cursor.fetchone():
+            conn.close()
+            return jsonify(status=400, message="Email already in use")
+
+        if discord:
+            cursor.execute("SELECT * FROM hackers WHERE discord = ?", (discord,))
+            if cursor.fetchone():
+                conn.close()
+                return jsonify(status=400, message="Discord already in use")
+        
+        cursor.execute("INSERT INTO hackers (teamID, firstName, lastName, password, email, school, discord, confirmationNumber, isConfirmed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+               (teamID, firstName, lastName, password, email, school, discord, confirmationNumber, isConfirmed))
         conn.commit()
+
+        hacker_id = cursor.lastrowid
+        new_hacker = get_hacker_by_id(hacker_id)
         conn.close()
 
-        return jsonify(status=201, message="Hacker created successfully")
+
+        return jsonify(status=201, message="Hacker created successfully", hacker=new_hacker)
     except Exception as e:
-     return jsonify(status=400, message=str(e))
+        return jsonify(status=400, message=str(e))
