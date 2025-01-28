753d7051aefd741ffc282c603d3aa8fab3f8540b
Jayden Nguyen <Jayden.vinh.nguyen@gmail.com>
2025-01-27 23:24:40 -0800

    Added password encryption

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 import sqlite3
 import uuid
+import bcrypt
 
 
 app = Flask(__name__)
@@ -19,6 +20,11 @@ def get_hacker_by_id(hacker_id):
     if hacker:
         return dict(hacker)
     return None
+
+def hash_password(password):
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed
 
 @app.route("/")
 def index():
@@ -42,9 +48,15 @@ def urmom():
 def create_hacker():
     try:
         data = request.get_json()
+
+        required_fields = ['firstName', 'lastName', 'password', 'email', 'school']
+        for field in required_fields:
+            if field not in data:
+                return jsonify(status=400, message=f"Missing {field}")
+
         firstName = data['firstName']
         lastName = data['lastName']
-        password = data['password']
+        password = hash_password(data['password'])
         email = data['email']
         school = data['school']
         discord = data.get('discord', None)
@@ -59,13 +71,13 @@ def create_hacker():
         cursor.execute("SELECT * FROM hackers WHERE email = ?", (email,))
         if cursor.fetchone():
             conn.close()
-            return jsonify(status=400, message="Email already in use")
+            return jsonify(status=409, message="Email already in use")
 
         if discord:
             cursor.execute("SELECT * FROM hackers WHERE discord = ?", (discord,))
             if cursor.fetchone():
                 conn.close()
-                return jsonify(status=400, message="Discord already in use")
+                return jsonify(status=409, message="Discord already in use")
         
         cursor.execute("INSERT INTO hackers (teamID, firstName, lastName, password, email, school, discord, confirmationNumber, isConfirmed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", 
                (teamID, firstName, lastName, password, email, school, discord, confirmationNumber, isConfirmed))
@@ -78,4 +90,4 @@ def create_hacker():
 
         return jsonify(status=201, message="Hacker created successfully", hacker=new_hacker)
     except Exception as e:
-        return jsonify(status=400, message=str(e))
+        return jsonify(status=500, message=str(e))
