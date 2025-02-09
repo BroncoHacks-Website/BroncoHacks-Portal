844e4d51031f5f931a432450c455ec57b91374a2
Nicholas Amancio <nick.amancio44@gmail.com>
2025-02-08 18:52:48 -0800

    finished all of the testing for the team functions. Found a way to exclude the teamID when testing since randomly generated id's were granted to teams when created which fails the tests. Made the init_db.py file one function so it can run in test_app.py and reset the db each time when running pytest -vv command

--- server/init_db.py
+++ server/init_db.py
@@ -1,36 +1,44 @@
 import sqlite3
 import bcrypt
 
-def hash_password(password):
-    salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed
+def run_db():
+    def hash_password(password):
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed
 
-connection = sqlite3.connect('database.db')
+    connection = sqlite3.connect('database.db')
 
 
-with open('schema.sql') as f:
-    connection.executescript(f.read())
+    with open('schema.sql') as f:
+        connection.executescript(f.read())
 
-cur = connection.cursor()
+    cur = connection.cursor()
 
-cur.execute("INSERT INTO hackers (teamID, firstName, lastName, password, email, school, discord, confirmationNumber, isConfirmed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-            (123456, 'Daniel', 'Pasion', hash_password('urmom'), 'dpasion@cpp,edu', 'cpp','.theDaniel', 666420, True )
-            )
+    cur.execute("INSERT INTO hackers (teamID, firstName, lastName, password, email, school, discord, confirmationNumber, isConfirmed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                (123456, 'Daniel', 'Pasion', hash_password('urmom'), 'dpasion@cpp,edu', 'cpp','.theDaniel', 666420, True )
+                )
 
-cur.execute("INSERT INTO hackers (teamID, firstName, lastName, password, email, school, discord, confirmationNumber, isConfirmed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-            (None, 'Cesar Henry', 'de Paula', hash_password('urdad'), 'chdp@cpp,edu', 'cpp','putanginamo', 666420, True ))
+    cur.execute("INSERT INTO hackers (teamID, firstName, lastName, password, email, school, discord, confirmationNumber, isConfirmed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                (None, 'Cesar Henry', 'de Paula', hash_password('urdad'), 'chdp@cpp,edu', 'cpp','putanginamo', 666420, True ))
 
-cur.execute("INSERT INTO hackers (teamID, firstName, lastName, password, email, school, discord, confirmationNumber, isConfirmed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-            (123456, 'LeCaleb', 'chung', hash_password('ursis'), 'lbj@cpp,edu', 'cpp','thebigtig', 666420, True ))
+    cur.execute("INSERT INTO hackers (teamID, firstName, lastName, password, email, school, discord, confirmationNumber, isConfirmed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                (123456, 'LeCaleb', 'chung', hash_password('ursis'), 'lbj@cpp,edu', 'cpp','thebigtig', 666420, True ))
 
-cur.execute("INSERT INTO teams (teamID, teamName, owner) VALUES (?, ?, ?)",
-            (123456, "Hawk Tuahers", 1)
-            )
+    cur.execute("INSERT INTO teams (teamID, teamName, owner) VALUES (?, ?, ?)",
+                (123456, "Hawk Tuahers", 1)
+                )
 
-cur.execute("UPDATE teams SET teamMember1 = 3 WHERE teamID = 123456")
+    cur.execute("INSERT INTO hackers (teamID, firstName, lastName, password, email, school, discord, confirmationNumber, isConfirmed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                (123457, 'nicek', 'ama', hash_password('ursis'), 'nja@cpp,edu', 'cpp','nicktehcan', 111111, True ))
+    
+    cur.execute("INSERT INTO teams (teamID, teamName, owner) VALUES (?, ?, ?)",
+                (919191, "chinatsu", 4)
+                )
 
-connection.commit()
-connection.close()
+    cur.execute("UPDATE teams SET teamMember1 = 3 WHERE teamID = 123456")
+
+    connection.commit()
+    connection.close()
 
 
--- server/test_app.py
+++ server/test_app.py
@@ -1,6 +1,7 @@
 import pytest
 import os
 from app import app
+from init_db import run_db
 import sqlite3
 import json
 
@@ -10,6 +11,8 @@ DB_PATH = os.path.abspath("database.db")
 def client():
     app.config["TESTING"] = True 
     #get the db to connect by running init_db.py first and then running pytest -vv
+    run_db()
+
     def test_db_connection():
         return sqlite3.connect(DB_PATH, check_same_thread=False)
     
@@ -18,26 +21,105 @@ def client():
     with app.test_client() as client:
         yield client
 
+def test_get_users_team(client):
+    response = client.get("/team?UUID=1")
+    assert response.json == {
+        "message": "success, we got them",
+        "owner": {
+            "UUID": 1,
+            "email": "dpasion@cpp,edu",
+            "firstName": "Daniel",
+            "lastName": "Pasion",
+            "school": "cpp"
+        },
+        "status": 200,
+        "team": {
+            "teamID": 123456,
+            "teamName": "Hawk Tuahers"
+        },
+        "teamMember1": {
+            "UUID": 3,
+            "email": "lbj@cpp,edu",
+            "firstName": "LeCaleb",
+            "lastName": "chung",
+            "school": "cpp"
+        },
+        "teamMember2": {
+            "UUID": None,
+            "email": None,
+            "firstName": None,
+            "lastName": None,
+            "school": None
+        },
+        "teamMember3": {
+            "UUID": None,
+            "email": None,
+            "firstName": None,
+            "lastName": None,
+            "school": None
+        }
+    }
+
+
 def test_create_tuah(client):
     data = {
         "teamName": "Hawk Tuaher",
         "owner": 2
     }
     response = client.post("/team", json=data)
+    response_json = response.json
+    assert response_json["message"] == "success !!!! Yippyyyyy"
+    assert response_json["status"] == 200
+    
+    team = response_json["team"]
+    assert team["owner"] == 2
+    assert "teamID" in team
+    assert isinstance(team["teamID"], str)
+    assert team["teamMember1"] == None
+    assert team["teamMember2"] == None
+    assert team["teamMember3"] == None
+    assert team["teamName"] == "Hawk Tuaher"
+    
+def test_delete_tuah(client):
+    data = {
+        "teamID": 919191,
+        "owner": 4 
+    }
+    response = client.delete("/team", json=data)
     assert response.status_code == 200
     assert response.json == {
-    "message": "success !!!! Yippyyyyy",
-    "status": 200,
-    "team": {
-        "owner": 2,
-        "teamID": "331745", #this will output fail but only because the randomly generated team id is causing it
-        "teamMember1": None,
-        "teamMember2": None,
-        "teamMember3": None,
-        "teamName": "Hawk Tuaher"
+        "message": "team deleted","status":"200"
     }
-}
-    
+
+def test_get_team(client):
+    response = client.get("/teams")
+    response_json = response.json
+
+    assert response_json["status"] == 200
+    assert response_json["message"] == "success"
+
+    team1 = response_json["teams"][0]
+    team2 = response_json["teams"][1]
+
+    assert team1["owner"] == "1"
+    assert "teamID" in team1
+    assert isinstance(team1["teamID"], int)
+    assert team1["teamMember1"] == "3"
+    assert team1["teamMember2"] == None
+    assert team1["teamMember3"] == None
+    assert team1["teamName"] == "Hawk Tuahers"
+
+    assert team2["owner"] == "2"
+    assert "teamID" in team2
+    assert isinstance(team2["teamID"], int)
+    assert team2["teamMember1"] == None
+    assert team2["teamMember2"] == None
+    assert team2["teamMember3"] == None
+    assert team2["teamName"] == "Hawk Tuaher"
+
+
+
+
 def test_get_all_data(client):
     response = client.get("/admin")
     assert response.status_code == 200
