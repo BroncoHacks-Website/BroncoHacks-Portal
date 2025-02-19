28968d8264036b394444d47515fdfc0f166b4788
DanielPasion <dnpaxion@gmail.com>
2025-02-18 20:53:27 -0800

    im tired af man lets just move to front end

--- server/init_db.py
+++ server/init_db.py
@@ -24,11 +24,11 @@ cur.execute("INSERT INTO hackers (teamID, firstName, lastName, password, email, 
 cur.execute("INSERT INTO hackers (teamID, firstName, lastName, password, email, school, discord, confirmationNumber, isConfirmed, isAdmin) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
             (123456, 'LeCaleb', 'chung', hash_password('ursis'), 'lbj@cpp,edu', 'cpp','thebigtig', 666420, True, False ))
 
-cur.execute("INSERT INTO hackers (teamID, firstName, lastName, password, email, school, discord, confirmationNumber, isConfirmed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-            (123456, 'Nick', 'Amancio', hash_password('urajfp'), 'namancio@cpp,edu', 'cpp','nickthecan', 666420, True ))
+cur.execute("INSERT INTO hackers (teamID, firstName, lastName, password, email, school, discord, confirmationNumber, isConfirmed, isAdmin) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            (123456, 'Nick', 'Amancio', hash_password('urajfp'), 'namancio@cpp,edu', 'cpp','nickthecan', 666420, True, True ))
 
-cur.execute("INSERT INTO hackers (teamID, firstName, lastName, password, email, school, discord, confirmationNumber, isConfirmed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-            (123456, 'Jade', 'Nguyen', hash_password('urdasfhp'), 'jgnuyen@cpp,edu', 'cpp','jadethegemstone', 666420, True ))
+cur.execute("INSERT INTO hackers (teamID, firstName, lastName, password, email, school, discord, confirmationNumber, isConfirmed, isAdmin) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            (123456, 'Jade', 'Nguyen', hash_password('urdasfhp'), 'jgnuyen@cpp,edu', 'cpp','jadethegemstone', 666420, True, True ))
 
 cur.execute("INSERT INTO teams (teamID, teamName, owner) VALUES (?, ?, ?)",
             (123456, "Hawk Tuahers", 1)
