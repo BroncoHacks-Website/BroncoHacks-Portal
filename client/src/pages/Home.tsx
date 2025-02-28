ba9a39f6f908975f9c4f1069b40aee9ad6e885f8
Nicholas Amancio <112116021+Nickthecan@users.noreply.github.com>
2025-02-27 20:36:05 -0800

    I didn't know we were suppose to work mobile responsiveness first and then work our way up to desktop ...

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,37 +1,22 @@
 import { Link } from "react-router";
-
+import "../index.css"
 function Home() {
   return (
     <>
-      <div className="bg-indigo-300 h-[85vh] flex flex-col">
-        <div className="flex flex-col h-[60vh] w-[30vw] m-auto rounded-lg bg-white">
-          <div className="mx-auto mt-5 text-xl">
-            BroncoHacks Official Regisration Portal
+        <div className="bg-indigo-300 h-[85vh] flex flex-col">
+          <div className="flex flex-col md:flex-row h-[vh70] w-[100vw] justify-center items-center m-auto">
+            <img src="BroncoHacksSquareLogo.png" className="h-[30vh] w-[30vh] mx-auto animate-[float_3s_infinite_alternate]" alt="BroncoHacks Logo"/>
+            <div className="w-[65vw]">
+              <div className="mx-auto text-[2rem] md:text-[6rem] mb-[10%] text-center font-bold overflow-hidden border-r-4 border-white whitespace-nowrap animate-[typing_7s_steps(30,end)_infinite]">Official Registration Portal</div>
+              <div className="flex justify-items:start ">
+                <Link className="mx-auto text-white text-[1.5rem] md:text-[2rem] bg-[#035BA5] hover:bg-gradient-to-r hover:from-blue-500 hover:to-green-300 transition-all duration-500 focus:outline-none focus:ring-4 focus:ring-blue-300 font-bold rounded-[20px] py-3 px-4 md:py-6 md:px-10 transform hover:scale-105"
+                  to={{pathname: "/Login",}}>Login</Link>
+                <Link className="mx-auto text-white text-[1.5rem] md:text-[2rem] bg-[#035BA5] hover:bg-gradient-to-r hover:from-blue-500 hover:to-green-300 transition-all duration-500 focus:outline-none focus:ring-4 focus:ring-blue-300 font-bold rounded-[20px] py-3 px-4 md:py-6 md:px-10 transform hover:scale-105"
+                  to={{pathname: "/CreateAccount",}}>Create Account</Link>
+              </div>
+            </div>
           </div>
-          <img
-            src="BroncoHacksSquareLogo.png"
-            className="h-[30vh] w-[30vh] mx-auto"
-            alt="BroncoHacks Logo"
-          />
-          <Link
-            className="mt-5 mx-auto text-white bg-[#035BA5] hover:bg-[#02498A] focus:outline-none focus:ring-4 focus:ring-blue-300 font-bold rounded-lg  py-2 px-4"
-            to={{
-              pathname: "/Login",
-            }}
-          >
-            Login
-          </Link>
-          <div className="mx-auto">---------------or---------------</div>
-          <Link
-            className="mx-auto text-white bg-[#035BA5] hover:bg-[#02498A] focus:outline-none focus:ring-4 focus:ring-blue-300 font-bold rounded-lg py-2 px-4"
-            to={{
-              pathname: "/CreateAccount",
-            }}
-          >
-            Create Account
-          </Link>
         </div>
-      </div>
     </>
   );
 }
