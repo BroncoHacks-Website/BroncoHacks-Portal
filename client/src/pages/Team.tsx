b210a774742f58e74edb48fca53954c45787d72e
DanielPasion <dnpaxion@gmail.com>
2025-03-01 06:54:21 -0800

    ch

--- client/src/pages/Team.tsx
+++ client/src/pages/Team.tsx
@@ -3,51 +3,51 @@ function Team() {
     <>
       <div className="bg-[#c3d3eb] h-[85vh] flex flex-col sm:flex-row justify-center">
         {/* create your team tab */}
-        <div className="w-full h-full flex flex-col md:flex-row items-center justify-center">
-          <div className="bg-white h-[35vh] w-[80vw] md:h-[45vh] md:w-[40vw] rounded-2xl border-lg shadow-lg mx-auto text-center">
+        <div className="w-full h-full flex flex-col xl:flex-row items-center justify-center">
+          <div className="bg-white h-[35vh] w-[80vw] md:w-[60vw] lg:w-[50vw] xl:h-[55vh] xl:w-[45vw] rounded-2xl border-lg shadow-lg mx-auto text-center">
             <img
               src="horse.png"
               alt="single user default image"
-              className="h-[15vh] w-[15vh] mx-auto scale-50"
+              className="h-[15vh] xl:h-[35vh] w-[15vh] xl:w-[35vh] mx-auto scale-50"
             />
-            <div className="flex items-center justify-center font-black text-3xl">
+            <div className="flex items-center justify-center font-black text-3xl xl:text-5xl">
               <h1>Create a Team</h1>
             </div>
 
             <div className="flex items-center justify-center font-black pt-2">
-              <label className="">Select a Team Name: </label>
+              <label className="xl:text-2xl">Select a Name: </label>
               <input type="text" className="border-b-1 ml-2 pl-2" />
             </div>
             <div className="flex items-center justify-center my-2">
               <input
                 type="submit"
                 value="Create New Team"
-                className="bg-[#97d9c3] h-[5vh] w-[40vw] rounded-xl text-white font-bold shadow-lg hover:cursor-pointer"
+                className="bg-[#97d9c3] h-[5vh] w-[40vw] md:w-[30vw] lg:w-[20vw] xl:w-[10vw] rounded-xl text-white font-bold shadow-lg hover:cursor-pointer"
               />
               <br />
               <br />
             </div>
           </div>
-          <div className="md:hidden">---------------or---------------</div>
+          <div className="xl:hidden">---------------or---------------</div>
           {/* join team via code tab */}
-          <div className="bg-white h-[35vh] w-[80vw] md:h-[60vh] md:w-[40vw] rounded-2xl border-lg shadow-lg mx-auto text-center">
+          <div className="bg-white h-[35vh] w-[80vw] md:w-[60vw] lg:w-[50vw] xl:h-[55vh] xl:w-[45vw] rounded-2xl border-lg shadow-lg mx-auto text-center">
             <img
               src="group.png"
               alt="single user default image"
-              className="h-[15vh] w-[15vh] mx-auto"
+              className="h-[15vh] xl:h-[35vh] [35vh] w-[15vh] xl:w-[35vh] mx-auto"
             />
-            <div className="flex items-center justify-center font-black text-3xl">
+            <div className="flex items-center justify-center font-black text-3xl xl:text-5xl">
               <h1>Join Team via Code</h1>
             </div>
 
-            <label htmlFor="">Enter Code: </label>
+            <label className="xl:text-2xl">Enter Code: </label>
             <input type="text" className="border-b-1 ml-2 pl-2" />
 
             <div className="flex items-center justify-center my-5">
               <input
                 type="submit"
                 value="Join"
-                className="bg-[#97d9c3] h-[5vh] w-[40vw] rounded-xl text-white font-bold shadow-lg hover:cursor-pointer"
+                className="bg-[#97d9c3] h-[5vh] w-[40vw] md:w-[30vw] lg:w-[20vw] xl:w-[10vw] rounded-xl text-white font-bold shadow-lg hover:cursor-pointer"
               />
               <br />
               <br />
