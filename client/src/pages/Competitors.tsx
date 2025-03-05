a3d61cf8a1975e2361839acbe7158693ac71e744
Cesar de Paula <cdepaula942@gmail.com>
2025-03-05 02:26:19 -0800

    created page listing registered competitors

--- client/src/pages/Competitors.tsx
+++ client/src/pages/Competitors.tsx
@@ -1,44 +1,35 @@
+import { useEffect, useState } from "react";
+import { uri } from "../App";
 
+interface Teams {
+  teamName: string,
+  owner: string,
+  teamMember1: string,
+  teamMember2: string,
+  teamMember3: string
+}
 
 function Competitors() {
 
-  const competitors = [
-    {
-      Name: "The Kickass Krab",
-      Owner: "Patrick Subaru",
-      Member1: "John Pork",
-      Member2: "Squidward Tortillini",
-      Member3: "Eugene Krabs"
-    },
-    {
-      Name: "The Los Angeles Lakers",
-      Owner: "Jeanie Buss",
-      Member1: "LeBron James",
-      Member2: "Luka Doncic",
-      Member3: "LeSperm James"
-    },
-    {
-      Name: "Cobra Kai",
-      Owner: "Johnny Lawrence",
-      Member1: "Miguel Diaz",
-      Member2: "Tory Nichols",
-      Member3: "Hawk Tuah"
-    },
-    {
-      Name: "Miyagi-Do",
-      Owner: "Daniel LaRusso",
-      Member1: "Samantha LaRusso",
-      Member2: "Robby Keene",
-      Member3: "Demitri Alexopoulos"
-    },
-    {
-      Name: "Blue Lock",
-      Owner: "Ego Jinpachi",
-      Member1: "Itoshi Rin",
-      Member2: "Isagi Yoichi",
-      Member3: null
+  const [teams, setTeams] = useState<Teams[]>([]);
+
+  useEffect(() => {
+    const fetchTeams = async () => {
+
+      try {
+        const res = await fetch(uri + `teamsApproved`, {
+          method: "GET",
+        });
+        const json = await res.json();
+        if (json["status"] !== 200) return;
+        setTeams(json["all_teams"])
+      } catch (e) {
+        console.log(e);
+      }
     }
-  ];
+    fetchTeams();
+  }, [])
+
 
   return (
     <>
@@ -47,30 +38,30 @@ function Competitors() {
           <div>
             <h1 className="text-5xl font-bold p-2">🐴Competitors🐴</h1>
           </div>
-          <table className="bg-gray-1 text-black m-2 text-center w-full border-collapse border border-gray-300 text-sm sm:text-base">
+          <table className="bg-gray-1 text-black m-2 text-center w-full border-collapse border-gray-300 text-sm sm:text-base">
             <thead>
               <tr className="bg-[#c3d3eb]">
-                <th className="border px-3 py-1 first:rounded-tl-lg last:rounded-tr-lg">Team Name</th>
-                <th className="border px-3 py-1">Owner</th>
-                <th className="border px-3 py-1">Teammate</th>
-                <th className="border px-3 py-1">Teammate</th>
-                <th className="border px-3 py-1">Teammate</th>
+                <th className="px-3 py-1 first:rounded-tl-lg">Team Name</th>
+                <th className="px-3 py-1">Owner</th>
+                <th className="px-3 py-1">Teammate</th>
+                <th className="px-3 py-1">Teammate</th>
+                <th className="px-3 py-1 last:rounded-tr-lg">Teammate</th>
               </tr>
             </thead>
             <tbody>
-              {competitors.map((team, index) => (
+              {teams && teams.map((team, index) => (
                 <tr className="odd:bg-gray-100 even:bg-white">
-                  <td key={index} className="hover:bg-gray-50 border px-3 py-1">{team.Name}</td>
-                  <td className="hover:bg-gray-50 border border px-3 py-1">{team.Owner}</td>
-                  <td className="hover:bg-gray-50 border border px-3 py-1">{team.Member1}</td>
-                  <td className="hover:bg-gray-50 border border px-3 py-1">{team.Member2}</td>
-                  <td className="hover:bg-gray-50 border border px-3 py-1">{team.Member3}</td>
+                  <td key={index} className="hover:bg-gray-50 px-3 py-1">{team.teamName}</td>
+                  <td className="hover:bg-gray-50 px-3 py-1">{team.owner}</td>
+                  <td className="hover:bg-gray-50 px-3 py-1">{team.teamMember1}</td>
+                  <td className="hover:bg-gray-50 px-3 py-1">{team.teamMember2}</td>
+                  <td className="hover:bg-gray-50 px-3 py-1">{team.teamMember3}</td>
                 </tr>
               ))}
             </tbody>
             <tfoot>
               <tr>
-                <td colSpan={5} className="bg-[#c3d3eb] border px-3 py-1 text-center first:rounded-bl-lg last:rounded-br-lg">
+                <td colSpan={5} className="bg-[#c3d3eb] font-bold px-3 py-1 text-center first:rounded-bl-lg last:rounded-br-lg">
                   End
                 </td>
               </tr>
